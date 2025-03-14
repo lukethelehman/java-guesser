12a9ae20d9f9c44dc99c0bf2a36da098799bbd66
lukethelehman <lukelehman05@gmail.com>
2025-03-14 18:12:26 -0400

    finished computer guesser

--- Guesser.java
+++ Guesser.java
@@ -14,10 +14,15 @@ public class Guesser {
 			if (response.equals("0")){
 				keepGoing = false;
 			}
-			if (response.equals("1")){
+			else if (response.equals("1")){
+				System.out.println();
+				System.out.println("Human Guesser");
 				humanGuesser();
 			}
-			if (response.equals("2")){
+			else if (response.equals("2")){
+				System.out.println();
+				System.out.println("Computer Guesser");
+				System.out.println("Think of a number between 1-100");
 				computerGuesser();
 			}
 		}
@@ -26,7 +31,7 @@ public class Guesser {
 	public String menu(){
 		System.out.println("0) Exit");
 		System.out.println("1) Human Guesser");
-		System.out.println("0) Computer Guesser");
+		System.out.println("2) Computer Guesser");
 		System.out.print("Please enter 0-2: ");
 		String response = input.nextLine();
 		return response;
@@ -38,21 +43,56 @@ public class Guesser {
 		boolean keepGoing = true;
 			while (keepGoing){
 				turns++;
-				System.out.print("Please enter a number: ");
+				System.out.print("Please enter a number between 1-100: ");
 				guess = input.nextInt();
+				input.nextLine();
 				if (guess == correct){
 					System.out.println("That's right! It took you " + turns + " turns.");
+					System.out.println();
 					keepGoing = false;
 				}
-				if (guess < correct){
+				else if (guess < correct){
 					System.out.println("Too low...");
 				}
-				if (guess > correct){
+				else if (guess > correct){
 					System.out.println("Too high...");
 				}
 			}
 	}
 	public void computerGuesser(){
-		System.out.println("computerGuesser");
+		int lowerBound = 1;
+		int upperBound = 100;
+		int guess = getMean(lowerBound, upperBound);
+
+		int turns = 0;
+		boolean keepGoing = true;
+
+		while (keepGoing){
+			turns++;
+			System.out.println(turns + ") I guess " + guess);
+			System.out.print("Too (H)igh, too (L)ow, or (C)orrect? ");
+			String response = input.nextLine();
+			if (response.equals("h")){
+				upperBound = guess;
+				guess = getMean(lowerBound, upperBound);
+			}
+			if (response.equals("l")){
+				lowerBound = guess;
+				guess = getMean(lowerBound, upperBound);
+			}
+			if (response.equals("c")){
+				System.out.println();
+				keepGoing = false;
+			}
+		}
+		
+	}
+
+
+
+
+	public int getMean(int lowerBound, int upperBound){
+		int mean = (lowerBound + upperBound) / 2;
+		return mean;
 	}
 }
